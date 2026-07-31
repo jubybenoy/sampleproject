c8ceb6e43db01d273c14334155ac5ba4b52f11d8
JUBY <jubimol.baby@gmail.com>
2026-07-31 23:10:12 +0530

    code edited

---
+++ basic5.js
@@ -0,0 +1,15 @@
+function display()
+{
+    console.log("Hello World");
+}
+display();  //function will execute only when we call it. otherwise it will not execute.
+
+const display1=function()
+{
+    console.log("Hello World");
+}
+display1();
+
+  const add=(a,b)=>a+b;
+  
+  console.log(add(10,20));
