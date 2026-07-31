473e779e74f23f7d4db5d6d1facd0cf1809d6d61
JUBY <jubimol.baby@gmail.com>
2026-07-31 23:25:26 +0530

    assignment code

--- assignment1.3.js
+++ assignment1.3.js
@@ -2,13 +2,16 @@ function datatype(value) {
   console.log(value, "=",typeof value);
 }
 
-datatype("Hello");             // String
+datatype("juby");             // String
 datatype(42);                  // Number
 datatype(true);                // Boolean
 datatype(undefined);           // Undefined
 datatype(null);                // Null (typeof shows "object")
-datatype(123n);                // BigInt
-datatype(Symbol("id"));        // Symbol
-datatype({ name: "Anu" });     // Object
+datatype(98765467n);                // BigInt
+datatype(Symbol("rollno"));        // Symbol
+datatype({ name: "miguel" });     // Object
 datatype([1, 2, 3]);           // Array (also type "object")
 datatype(function fruits(){});  // Function
+
+
+
