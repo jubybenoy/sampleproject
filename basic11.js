a41756e59d80796e47b15a25f069cf95e9a5ace1
JUBY <jubimol.baby@gmail.com>
2026-08-04 13:35:36 +0530

    array filter with condition

---
+++ basic11.js
@@ -0,0 +1,7 @@
+let numbers=[1,2,3,4,5,6,7,8,9,10];
+let result=numbers.filter(num=> num%2==0);
+console.log(result);
+
+let marks=[35,40,45,50,55,60,65,70,75,80];
+let passedstudents=marks.filter(m1=>m1>50);
+console.log(passedstudents);
