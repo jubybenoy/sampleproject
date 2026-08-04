8c55aae87f4f095710f3f798a0533b436bf013c6
JUBY <jubimol.baby@gmail.com>
2026-08-04 13:28:37 +0530

    array keysyntax2

---
+++ basic8.js
@@ -0,0 +1,27 @@
+let boys=["manu","arun"];
+let girls=["anu","anju"];
+let students=boys.concat(girls); //concat or combine 2 arrays
+console.log (students);
+let numbers=[1,4,3,4,5,6,7,8,9,11,22,33,44,55,66,77];
+let result=numbers.slice(0,3);
+console.log(result);
+
+let fruits=["apple","orange","mango"];
+fruits.reverse();
+console.log(fruits);
+fruits.sort();
+console.log(fruits);
+fruits.splice(0,3);
+console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
