17b7d0c8eb88169e153d5d168cc5595297f2095f
JUBY <jubimol.baby@gmail.com>
2026-08-04 12:58:04 +0530

    typeof

---
+++ basic3.js
@@ -0,0 +1,21 @@
+let studenname="nimmy";
+console.log(typeof studentname);
+let mark=95;
+console.log(typeof mark);
+let ispass=true;
+console.log(typeof ispass);
+let age;
+console.log(typeof age);
+let salary=null;
+console.log(typeof salary);
+let students=["Java","Python","playwright"];
+typeof students;
+let employee={
+    id:101,
+    name:"Anu",
+    department:"QA"
+};
+console.log(employee);
+let id=Symbol("employee");
+let id2=Symbol("employee");
+console.log(id===id2);  // === stright equal. will check datatype too.
