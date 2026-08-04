99948c1f38bdbe0262552aabacac7abe6a1e25b4
JUBY <jubimol.baby@gmail.com>
2026-08-04 13:29:44 +0530

    foreach method syntax

---
+++ basic9.js
@@ -0,0 +1,17 @@
+
+
+
+ //foreach method syntax
+/*array.forEach((element,index)=>{
+
+})*/
+
+const numbers=[10,20,30];
+numbers.forEach((num)=>{
+    console.log(num)
+})
+
+const fruits=["Apple","Orange","Grape"];
+fruits.forEach((fruit,index)=>{
+    console.log(index +" "+fruit);
+})
